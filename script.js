076db9fd287b394bbb57621aa49e6dc6f0037756
motty mandel <mandelmottyisrael65@gmail.com>
2024-12-16 17:36:59 -0500

    added a login popup

---
+++ script.js
@@ -0,0 +1,9 @@
+function getForm() {
+    document.getElementById('popup').style.display = 'flex';
+    document.getElementById('overlay').style.display = 'block';
+}
+
+function hideForm() {
+    document.getElementById('popup').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
+}
