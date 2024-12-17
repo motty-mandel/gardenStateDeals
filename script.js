0c107d4eb76e503548538b3ab6273861a966e4ad
motty mandel <mandelmottyisrael65@gmail.com>
2024-12-17 15:39:21 -0500

    added a login page

--- script.js
+++ script.js
@@ -6,4 +6,15 @@ function getForm() {
 function hideForm() {
     document.getElementById('popup').style.display = 'none';
     document.getElementById('overlay').style.display = 'none';
+    document.getElementById('create-account').style.display = 'none';
+}
+
+function createAccount() {
+    document.getElementById('popup').style.display = 'none';
+    document.getElementById('create-account').style.display = 'flex';
+}
+
+function goBack() {
+    document.getElementById('create-account').style.display = 'none';
+    document.getElementById('popup').style.display = 'flex';
 }
