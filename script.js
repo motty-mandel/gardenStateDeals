4e453542503e29e7cc3bb48abc958ff0b7babf0e
motty mandel <mandelmottyisrael65@gmail.com>
2024-12-26 20:05:14 -0500

    added a few new animations

--- script.js
+++ script.js
@@ -7,14 +7,20 @@ function hideForm() {
     document.getElementById('popup').style.display = 'none';
     document.getElementById('overlay').style.display = 'none';
     document.getElementById('create-account').style.display = 'none';
+    document.getElementById('error').style.display = 'none';
 }
 
 function createAccount() {
     document.getElementById('popup').style.display = 'none';
     document.getElementById('create-account').style.display = 'flex';
+    document.getElementById('error').style.display = 'none';
 }
 
 function goBack() {
     document.getElementById('create-account').style.display = 'none';
     document.getElementById('popup').style.display = 'flex';
+}
+
+function error() {
+    document.getElementById('error').style.display = 'block';
 }
